89837df458c265a8fe265892a2643117f8ba0cc1
krkrishna123 <krishnakumar3454878@gmail.com>
2023-07-06 21:28:11 +0530

    In the user Entity some changes done by Krishna

--- src/main/java/com/lcwd/electronic/store/entity/User.java
+++ src/main/java/com/lcwd/electronic/store/entity/User.java
@@ -25,10 +25,10 @@ public class User {
     @Column(name = "user_name")
     private String name;
 
-    @Column(name="user_email",unique=true)
+    @Column(name="user_email")
     private String email;
 
-    @Column(name="user_password",length=10)
+    @Column(name="user_password")
     private String password;
 
     private String gender;
