5e52899d8fd6e5654b33f79eed6041273b5aeae4
krkrishna123 <krishnakumar3454878@gmail.com>
2023-07-15 13:21:20 +0530

    logger are implemented in every Api in Category controller.

--- src/main/java/com/lcwd/electronic/store/controller/CategoryController.java
+++ src/main/java/com/lcwd/electronic/store/controller/CategoryController.java
@@ -4,6 +4,8 @@ import com.lcwd.electronic.store.dto.ApiResponseMessage;
 import com.lcwd.electronic.store.dto.CategoryDto;
 import com.lcwd.electronic.store.dto.PageableResponse;
 import com.lcwd.electronic.store.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    private static Logger logger=  LoggerFactory.getLogger( CategoryController.class);
     /**
      * auhor's name :krishna
      * @param Category related all controllerApi
@@ -32,7 +35,11 @@ public class CategoryController {
     @PostMapping
    public ResponseEntity<CategoryDto>createCategory(@RequestBody CategoryDto categoryDto){
        //call service to save object
+        logger.info(" Before initiated createdCategory in controller :="+ categoryDto);
+
        CategoryDto categoryDto1 = categoryService.create(categoryDto);
+
+        logger.info(" after initiated createdCategory in controller :="+ categoryDto);
        return new ResponseEntity<>(categoryDto1, HttpStatus.CREATED);
    }
     //update
@@ -48,7 +55,9 @@ public class CategoryController {
            @PathVariable String categoryId,
             @RequestBody CategoryDto categoryDto
     ){
+        logger.info(" Before initiated updatedCategory in controller by categoryId :="+ categoryId);
         CategoryDto updatedCategory = categoryService.update(categoryDto, categoryId);
+        logger.info(" after initiated updatedCategory in controller by categoryId :="+ categoryId);
         return new ResponseEntity<>(updatedCategory,HttpStatus.OK);
     }
     //delete
@@ -60,16 +69,17 @@ public class CategoryController {
      */
     @DeleteMapping("/{categoryId}")
     public ResponseEntity<ApiResponseMessage>deleteCategory(@PathVariable String categoryId){
-categoryService.delete(categoryId);
+        logger.info(" Before initiated Delete operation Category in controller by categoryId :="+ categoryId);
+         categoryService.delete(categoryId);
         ApiResponseMessage responseMessage = ApiResponseMessage.builder().message("Category is delete  successfully!!!!").status(HttpStatus.OK).success(true).build();
-   return new ResponseEntity<>(responseMessage,HttpStatus.OK);
+        logger.info(" After initiated Delete operation Category in controller by categoryId :="+ categoryId);
+        return new ResponseEntity<>(responseMessage,HttpStatus.OK);
 
     }
     //getall
 
     /**
-     * apiNotes:Get All
-     * @param Pagination and sorting for this
+     * apiNotes:Get All by pagination and sorting 
      * @param pageNumber
      * @param pageSize
      * @param sortBy
@@ -84,7 +94,9 @@ public ResponseEntity<PageableResponse<CategoryDto>>getAll(
         @RequestParam(value="sortBy",defaultValue = "title",required=false)String sortBy,
         @RequestParam(value="sortDir",defaultValue = "asc",required=false)String sortDir
 ){
+        logger.info(" Before operation of Pagination and sorting by GetAll operation in Category Controller");
     PageableResponse<CategoryDto> pageableResponse = categoryService.getAll(pageNumber, pageSize, sortBy, sortDir);
+        logger.info(" After operation of Pagination and sorting by GetAll operation in Category Controller");
     return new ResponseEntity<>(pageableResponse,HttpStatus.OK);
     }
     //get single
@@ -96,7 +108,9 @@ public ResponseEntity<PageableResponse<CategoryDto>>getAll(
      */
     @GetMapping("/{categoryId}")
     public ResponseEntity<CategoryDto>getSingle(@PathVariable String categoryId){
+        logger.info(" Before operation of get single in CategoryController by id" + categoryId);
         CategoryDto categoryDto = categoryService.get(categoryId);
+        logger.info(" after operation of get single in CategoryController " + categoryId);
         return ResponseEntity.ok(categoryDto);
     }
 
