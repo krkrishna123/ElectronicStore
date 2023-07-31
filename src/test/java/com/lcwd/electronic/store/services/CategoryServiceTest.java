f05b1ca46b830cda723f0a259dfc70f0f31d0263
krkrishna123 <krishnakumar3454878@gmail.com>
2023-07-31 19:30:21 +0530

    Test case passed for getAllcategory method  in the categoryService .

--- src/test/java/com/lcwd/electronic/store/services/CategoryServiceTest.java
+++ src/test/java/com/lcwd/electronic/store/services/CategoryServiceTest.java
@@ -1,7 +1,10 @@
 package com.lcwd.electronic.store.services;
 
 import com.lcwd.electronic.store.dto.CategoryDto;
+import com.lcwd.electronic.store.dto.PageableResponse;
+import com.lcwd.electronic.store.dto.UserDto;
 import com.lcwd.electronic.store.entity.Category;
+import com.lcwd.electronic.store.entity.User;
 import com.lcwd.electronic.store.repository.CategoryRepository;
 import com.lcwd.electronic.store.service.CategoryService;
 import org.junit.jupiter.api.Assertions;
@@ -12,7 +15,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -74,7 +82,31 @@ public void createCategoryTest(){
         Mockito.when(categoryRepository.findById("userIdabc")).thenReturn(Optional.of(category));
         categoryService.delete(categoryid);
         Mockito.verify(categoryRepository,Mockito.times(1)).delete(category);
+    }@Test
+    public void getAllCategoryTest(){
+        Category category1= Category.builder()
+                .title("This is testing1 for GetAllCategory Method")
+                .coverImage("kks.png")
+                .description("lcwd")
+                .build();
+        Category category2= Category.builder()
+
+                .title("This is testing2 for GetAllCategory Method")
+                .coverImage("pkp.png")
+                .description("lcwd")
+                .build();
+
+        List<Category> categoryList= Arrays.asList(category1,category2);
+
+        Page<Category> page=new PageImpl<>(categoryList);
+        Mockito.when(categoryRepository.findAll((Pageable)Mockito.any())).thenReturn(page);
+
+//       Sort sort=Sort.by("name").ascending();
+//        Pageable pageable= PageRequest.of( 1, 2, sort);
+//
+        PageableResponse<CategoryDto> allCategory=categoryService.getAll(1,2,"title","asc");
+        Assertions.assertEquals(2,allCategory.getContent().size());
+
     }
-    
     }
 
